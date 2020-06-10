5594e293ecd23f1bb64353f4d46351a75eb19fcb
Jesse Jacobson <jesseajacobson47@gmail.com>
2020-06-09 22:04:19 -0600

    fixed bug that let player leave name blank

--- assests/js/game.js
+++ assests/js/game.js
@@ -6,7 +6,6 @@ var randomNumber = function(min, max) {
 
 // function to start a new game
 var startGame = function() {
-    // reset player stats
     playerInfo.reset();
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
@@ -128,8 +127,18 @@ var shop = function() {
     }
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
